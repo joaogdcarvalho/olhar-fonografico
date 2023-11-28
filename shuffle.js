1b4db3b9d8f417a273ef54d4f58c1cc6cae802aa
João G. de Carvalho <27590118+joaogdcarvalho@users.noreply.github.com>
2023-11-28 20:53:04 -0300

    refactor(shuffle): atualiza nome função para linkAleatorio

--- shuffle.js
+++ shuffle.js
@@ -2,7 +2,7 @@
 const vitrineURLs = Array.from({ length: 100 }, (_, i) => `vitrine${i + 1}.html`);
 
 // Função para redirecionar para um URL aleatório
-function redirecionarParaURLAleatorio() {
+function linkAleatorio() {
    // Gera um número aleatório dentro dos limites do array de URLs
    const indiceAleatorio = Math.floor(Math.random() * vitrineURLs.length);
    
